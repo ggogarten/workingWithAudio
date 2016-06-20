da9d1f31f3f1541307980e25e469b65f4084276c
George G <ggogarten@gmail.com>
2016-06-20 17:35:18 -0400

    added additional stop button

--- Working With Audio/ViewController.swift
+++ Working With Audio/ViewController.swift
@@ -7,12 +7,84 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        player.pause()
+        
+        
+    }
+    
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        player.pause()
+        
+        let audioPath = NSBundle.mainBundle().pathForResource("backInTheDay", ofType: ".mp3")!
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+            
+            
+            
+        } catch {
+            
+            print("error")
+            
+        }
+
+        
+    }
+    
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        player.play()
+        
+        
+    }
+    
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+    
+    player.volume = slider.value
+    
+    
+    }
+    
+    
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = NSBundle.mainBundle().pathForResource("backInTheDay", ofType: ".mp3")!
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+            
+            
+            
+        } catch {
+            
+            print("error")
+            
+        }
+
+      
+    
     }
 
     override func didReceiveMemoryWarning() {
